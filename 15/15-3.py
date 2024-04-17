81765c8564dfe6c6174638e571f5ac02e6a5aceb
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-04-17 20:18:05 +0300

    15_3 made line thinner

--- 15/15-3.py
+++ 15/15-3.py
@@ -8,7 +8,7 @@ rw.fill_walk()
 plt.style.use("classic")
 fig, ax = plt.subplots()
 point_numbers = range(rw.num_points)
-ax.plot(rw.x_values, rw.y_values, linewidth = 3)
+ax.plot(rw.x_values, rw.y_values, linewidth = 2)
 
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
