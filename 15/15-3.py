d733ad0f17411a404b5a13c2e5562fb1dcf2b65c
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-04-17 20:15:07 +0300

    15th directory updated

---
+++ 15/15-3.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.style.use("classic")
+fig, ax = plt.subplots()
+point_numbers = range(rw.num_points)
+ax.plot(rw.x_values, rw.y_values, linewidth = 3)
+
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax.set_aspect("equal")
+plt.show()
