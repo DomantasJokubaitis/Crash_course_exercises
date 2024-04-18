bff3816e4f0f513b8cc02db4cf927d8648c85edf
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-04-18 19:46:58 +0300

    update

--- 15/15-7.py
+++ 15/15-7.py
@@ -2,22 +2,23 @@ import plotly.express as px
 
 from rolling_dice import Die
 
-die_1 = Die(8)
-die_2 = Die(8)
+die_1 = Die(6)
+die_2 = Die(6)
+die_3 = Die(6)
 
 results = []
 
-for number in range(100000):
-    result = die_1.roll_dice() + die_2.roll_dice()
+for number in range(10000):
+    result = die_1.roll_dice() + die_2.roll_dice() + die_3.roll_dice()
     results.append(result)
 
 frequencies = []
-max_result = die_1.sides + die_2.sides
+max_result = die_1.sides + die_2.sides + die_3.sides
 poss_results = range(2, max_result + 1)
 
-
-frequency = [results.count(value) for value in poss_results]
-frequencies.append(frequency)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
 
 title = "Results of rolling two dice"
 labels = {"x": "result", "y": "frequency of result"}
