c4e3e6358171260cb2f28d0795372c728d36429d
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-04-20 19:11:39 +0300

    update

--- not_specified/tampa_temperature_change.py
+++ not_specified/tampa_temperature_change.py
@@ -17,8 +17,10 @@ for title in header:
 
 
 dates_1980 = []
+dates_1980_str = []
 temps_1980 = []
 dates_2000 = []
+dates_2000_str = []
 temps_2000 = []
 dates_2020 = []
 temps_2020 = []
@@ -26,21 +28,42 @@ for line in reader:
     try:
         date = line[2]
         temp = line[8]
+
         if "1980" in date:
             temps_1980.append(int(temp))
+            dates_1980_str.append(date)
             dates_1980.append(dt.strptime(date, "%Y-%m-%d"))
-        elif "2000" in date:
-            temps_2000.append(int(temp))
-            dates_2000.append(dt.strptime(date, "%Y-%m-%d"))
-        elif "2020" in date:
-            temps_2020.append(int(temp))
-            dates_2020.append(dt.strptime(date, "%Y-%m-%d"))
-        else:
-            continue
+
+        if "2000" in date:
+            for item in dates_1980_str:
+                if item[5:] in date:
+                    temps_2000.append(int(temp))
+                    dates_2000_str.append(date)
+                    dates_2000.append(dt.strptime(date, "%Y-%m-%d"))
+                else:
+                    continue
+
+
+        for item in dates_1980_str:
+            if item[5:] not in dates_2000_str:
+                dates_1980_str.remove(item)
+
+
+        if "2020" in date:
+            for item in dates_1980_str:
+                if item[5:] in date:
+                    temps_2020.append(int(temp))
+                    dates_2020.append(dt.strptime(date, "%Y-%m-%d"))
+                else:
+                    continue
+
     except ValueError:
         print("skipping...")
         continue
 
+print(len(dates_1980))
+print(len(dates_2000))
+print(len(dates_2020))
 plt.style.use("dark_background")
 
 fig, ax = plt.subplots()
@@ -53,7 +76,7 @@ ax.xaxis.set_major_formatter(hfmt)
 plt.xlabel('Date')
 plt.ylabel('Value')
 
-ax.plot(dates_1980, temps_1980, color="green")
+ax.plot(xaxis, temps_1980, color="green")
 ax.plot(dates_2000, temps_2000, color="yellow")
 ax.plot(dates_2020, temps_2020, color="red")
 #ax.get_xaxis().set_visible(False)
