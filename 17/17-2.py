56ebbf87c8e5b717db80271883bf287ecae052b0
Domantas Jokūbaitis <domas.jokubaitis@gmail.com>
2024-04-27 20:59:15 +0300

    getting ready

--- 17/17-2.py
+++ 17/17-2.py
@@ -9,7 +9,7 @@ print(f"Status code: {r.status_code}")
 submission_ids = r.json()
 submission_dicts = []
 
-for submission_id in submission_ids[:8]:
+for submission_id in submission_ids[:6]:
     url = url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
     response_dict = r.json()
@@ -27,7 +27,18 @@ for submission_id in submission_ids[:8]:
 
 submission_dicts = sorted(submission_dicts, key=itemgetter("comments"), reverse=True)
 
+titles, comments = [], []
+
 for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict["title"]}")
+    titles.append(submission_dict["title"])
     print(f"Discussion link: {submission_dict["hn_link"]}")
     print(f"Comments: {submission_dict["comments"]}")
+    comments.append(submission_dict["comments"])
+
+
+labels = {"x": "Title", "y": "Comments"}
+title = "Hacker news popular posts"
+fig = px.bar(x = titles, y = comments, labels = labels, title = title)
+
+fig.show()
